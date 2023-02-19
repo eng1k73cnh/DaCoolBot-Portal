3ee9a0eabfc431a4bee6f9da1d81d9466d5dd96b
Nam Anh <phamnamanh25@gmail.com>
2023-02-19 18:53:15 +0700

    chore: set up metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,14 +11,37 @@ export default function App({
     <SessionProvider session={session}>
       <Head>
         <title>DaCoolBot Portal</title>
-        <meta name="description" content="idk what i'm doing" />
+        <meta name="description" content="Web portal for DaCoolBot" />
         <meta name="og:type" content="website" />
         <meta name="og:url" content="https://dacoolbot.kyrie25.me/" />
         <meta name="og:title" content="DaCoolBot Portal" />
         <meta name="og:image" content="/icon.png" />
+        <meta content="summary" name="twitter:card" />
+        <meta content="dacoolbot.kyrie25.me" name="twitter:site" />
+        <meta content="DaCoolBot Portal" name="twitter:title" />
+        <meta content="Web portal for DaCoolBot" name="twitter:description" />
+        <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          sizes="180x180"
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
